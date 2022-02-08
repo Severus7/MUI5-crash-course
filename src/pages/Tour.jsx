fb8dd5ccdc20e1492dfe42a6816b517ff4ab3bea
Severus7 <joshuasapz@gmail.com>
2022-02-09 01:03:26 +0800

    first commit

---
+++ src/pages/Tour.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, BottomNavigation, Container, Paper, Typography } from "@mui/material";
+import ImageCollage from '../components/ImageCollage';
+import CustomizedAccordions from "../components/Accordion";
+import BasicModal from "../components/Modal";
+
+const Tour = () => {
+  return (
+    <Container sx={{ width: 900 }}>
+      <Typography variant="h3" component="h1" marginTop={3}>
+        Explore the World in Vegas
+      </Typography>
+      <Box marginTop={3} sx={{ display: "flex" }}>
+        <img
+          src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+          alt=""
+          height={325}
+        />
+        <ImageCollage />
+      </Box>
+      <Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+            About this ticket
+        </Typography>
+        <Typography variant="paragraph" component="p" marginTop={3}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a necessitatibus ducimus soluta at voluptas voluptates officiis suscipit, optio debitis. Commodi officia porro culpa vel qui fugit consectetur ad ipsam!
+        </Typography>
+      </Box>
+      <Box marginBottom={10}>
+        <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+            Frequently Asked Questions (FAQs)
+        </Typography>
+        <CustomizedAccordions />
+      </Box>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation showLabels>
+            <BasicModal />
+        </BottomNavigation>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Tour;
